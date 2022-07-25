89afd613b49d29b11e24c9500dde953a3c48b84e
shahab sharif <shahab.sharif@devsinc.com>
2022-07-25 18:26:30 +0500

    ^

--- collection.swift
+++ collection.swift
@@ -9,4 +9,18 @@ ar += [5]
 
 ar.remove(at:0)
 ar.append(8)
-print(ar)
+//print(ar)
+
+
+var dic = Dictionary<String,String>()
+
+dic["Name"] = "Shahab"
+print(dic["Name"]!)
+
+
+
+var dic2 = [String:Int]()
+dic2["Tyres"] = 5
+dic2["Steerings"] = 2
+
+print(dic2)
