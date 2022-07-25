d809a97da6180c18712e09845c0b85b090f713a0
shahab sharif <shahab.sharif@devsinc.com>
2022-07-25 16:23:22 +0500

    Optionals

--- hello.swift
+++ hello.swift
@@ -1,2 +1,16 @@
 
 
+
+var name:String?
+
+name = "Shahab"
+
+if let theName = name { //This line will make sure if there is a value inside the name box. If yes then the name will be automatically wrapped and the value will be assigned theName
+  print("we get a Name: \(theName)")
+}
+else{
+  print("We didn't get a name")
+}
+
+
+
